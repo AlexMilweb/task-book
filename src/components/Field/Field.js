83678902134f3b819a68e12b1707798246b655c7
Alexander Miliutin <milya89@gmail.com>
2019-03-20 01:45:48 +0200

    add ref to field

--- src/components/Field/Field.js
+++ src/components/Field/Field.js
@@ -2,13 +2,14 @@ import React from "react";
 import { SearchIcon } from "./icons/SearchIcon";
 import { FieldWrapper, FieldStyled, SearchIconStyled } from "./Field.styled";
 
-export const Field = ({ placeholder, onChange, value, isSearch }) => {
+export const Field = ({ placeholder, onChange, value, isSearch, ref }) => {
   return (
     <FieldWrapper>
       <FieldStyled
         placeholder={placeholder}
         value={value}
         onChange={onChange}
+        ref={ref}
       />
       {isSearch && <SearchIconStyled as={SearchIcon} width={20} height={20} />}
     </FieldWrapper>

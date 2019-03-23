dc6cf35e7cde313f31db85ac7552db76fc2f9857
Alexander Miliutin <milya89@gmail.com>
2019-03-23 23:19:19 +0200

    Updates

---
+++ precache-manifest.17ba587bf97431610e183b9f49ea2b8d.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "1b56059394bafd908e6f",
+    "url": "/task-book/static/js/runtime~main.6cc9eead.js"
+  },
+  {
+    "revision": "52e60bd251a8b2cd72ff",
+    "url": "/task-book/static/js/main.8072d8bb.chunk.js"
+  },
+  {
+    "revision": "45b5ce6282db7ffb15ea",
+    "url": "/task-book/static/js/2.fbb26b79.chunk.js"
+  },
+  {
+    "revision": "8e6b6326f4c1482930fa7028943c8dbe",
+    "url": "/task-book/index.html"
+  }
+];
